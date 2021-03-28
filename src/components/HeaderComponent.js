3b3fa5d76ae723cf378fe684de342f30ec0b77eb
IvanGV92 <ianlee9204@gmail.com>
2021-03-28 18:54:13 -0500

    [add] new version

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,21 +7,13 @@ class Header extends Component {
         super(props);
         this.state = {
             isNavOpen: false,
-            isModalOpen: false
         };
         this.toggleNav = this.toggleNav.bind(this);
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleLogin = this.handleLogin.bind(this);
-        
+
     }
 
 
-    handleLogin(event){
-        this.toggleModal();
-        alert("Username: "+ this.username.value + " Password: " + this.password.value3 + " Remember : "+ this.remember.checked);
-        event.preventDefault();
-        
-        }
+
 
     toggleNav(){
         this.setState({
@@ -29,11 +21,6 @@ class Header extends Component {
         });
     }
 
-    toggleModal(){
-        this.setState({
-            isModalOpen: !this.state.isModalOpen
-        });
-    }
 
     render(){
 
@@ -44,74 +31,33 @@ class Header extends Component {
                         <NavbarToggler onClick={this.toggleNav} />
                         <NavbarBrand className="mr-auto" href="/">
                             <img src="assets/images/logo.png" height="30" width="41"
-                                alt="Ristorante con Fusion" /> 
+                                alt="Ristorante con Fusion" />
                         </NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/home">
-                                        <span className="fa fa-home fa-lg"></span> Home  
+                                        <span className="fa fa-home fa-lg"></span> Home
                                     </NavLink>
                                 </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/aboutus">
-                                        <span className="fa fa-info fa-lg"></span> About Us  
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/menu">
-                                        <span className="fa fa-list fa-lg"></span> Menu  
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/contactus">
-                                        <span className="fa fa-address-card fa-lg"></span> Contact Us  
-                                    </NavLink>
-                                </NavItem>
+
                             </Nav>
-                            <Nav className ="ml-auto" navbar>
-                                <NavItem>
-                                    <Button outline style={{color: '#FFFFFF', borderColor:'#FFFFFF'}} onClick={this.toggleModal}>
-                                        <span className="fa fa-sign-in fa-lg"></span>Login
-                                    </Button>
-                                </NavItem>
-                            </Nav>
+
                         </Collapse>
                         </div>
                     </Navbar>
-                
+
                 <Jumbotron>
                     <div className="container">
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
-                                <h1>Ristorante Con Fusion</h1>
-                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                                <h1>Ivan's Market</h1>
+
                             </div>
                         </div>
-                    </div>  
+                    </div>
                 </Jumbotron>
-                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
-                    <ModalBody>
-                        <Form onSubmit={this.handleLogin}>
-                            <FormGroup>
-                                <Label htmlFor="username" >Username</Label>
-                                <Input type="text" id="username" name="username" innerRef={(input)=> this.username = input}></Input>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label htmlFor="password" >Password</Label>
-                                <Input type="password" id="password" name="password" innerRef={(input)=> this.password = input}></Input>
-                            </FormGroup>
-                            <FormGroup check>
-                                <Label check>
-                                    <Input type="checkbox" name="remember" innerRef={(input)=> this.remember = input}/>
-                                    Remember Me
-                                </Label>
-                            </FormGroup>
-                            <Button type="submit" value="submit" className="bg-primary">Login</Button>
-                        </Form>
-                    </ModalBody>
-                </Modal>
+
             </>
         );
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,111 +1,85 @@
 
 
 import React,{ Component } from 'react';
-import Home from './HomeComponent';
-import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import Contact from './ContactComponent';
-import About from './AboutComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders, postFeedback }  from '../redux/ActionCreators';
-import { actions } from 'react-redux-form';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Switch, Route, withRouter } from 'react-router-dom';
 
 
 
-const mapStateToProps = state =>{
-  return {
-    dishes: state.dishes,
-    comments: state.comments,
-    promotions: state.promotions,
-    leaders: state.leaders
-  }
-}
 
-const mapDispatchToProps = (dispatch) =>({
-  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())},
-  resetFeedbackForm: ()=> { dispatch(actions.reset('feedback'))},
-  fetchPromos: () => {dispatch(fetchPromos())},
-  fetchComments: () => {dispatch(fetchComments())},
-  fetchLeaders: () => {dispatch(fetchLeaders())},
-  postFeedback: (values) => postFeedback(values),
-  
-  
-  
-});
+
+
 
 
 
 class Main extends Component {
 
-  // eslint-disable-next-line
+
   constructor(props){
     super(props);
-  } 
+    this.state= {
+      products:[],
+      isLoading: true,
+      error: null
+        };
+  }
 
   componentDidMount(){
-    this.props.fetchDishes();
-    this.props.fetchComments();
-    this.props.fetchPromos();
-    this.props.fetchLeaders();
-    
+
+
+    fetch('http://localhost:8080/api/')
+      .then(response=> response.json())
+        .then(products=> {
+          this.setState({ products, isLoading:false, error:null });
+        })
+      .catch(error => this.setState({ products:[], error, isLoading: false }));
+
+
+
+
   }
- 
+
 
   render(){
+    const { products, isLoading, error } = this.state;
 
-    const HomePage = ()=>{
-      return(
-        <Home dish={this.props.dishes.dishes.filter((dish)=>dish.featured)[0]}
-        dishesLoading={this.props.dishes.isLoading}
-        dishesErrorMessage={this.props.dishes.errorMessage}
-        promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
-        promoLoading={this.props.promotions.isLoading}
-        promoErrorMessage={this.props.promotions.errorMessage}
-        leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
-        leaderLoading={this.props.leaders.isLoading}
-        leaderErrorMessage={this.props.leaders.errorMessage}/>
-        
-        //leader={this.props.leaders.filter((leader)=>leader.featured)[0]}/>
-      );
+    if(error){
+      return <p>{error.message}</p>;
     }
-
-    const DishWithId = ({ match })=>{
-      return(
-        <DishDetail dish={this.props.dishes.dishes.filter((dish)=>dish.id === parseInt(match.params.dishId,10))[0]} 
-        isLoading={this.props.dishes.isLoading}
-        errorMessage={this.props.dishes.errorMessage}
-        comments={this.props.comments.comments.filter((comment)=> comment.dishId === parseInt(match.params.dishId,10))}
-        commentsErrorMessage={this.props.comments.errorMessage}
-        postComment={this.props.postComment}
-        />
-      );
-    }
-
-    return (
-      <div>
+    if(isLoading){
+      return (
+        <div>
           <Header />
-          <TransitionGroup>
-            <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
-              <Switch>
-                <Route path="/home" component={HomePage} />
-                <Route exact path="/aboutus" component={()=><About leaders={this.props.leaders}/>} />
-                <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}/>} />
-                <Route path="/menu/:dishId" component={DishWithId} />
-                <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} postFeedback={this.props.postFeedback}/>} />
-                <Redirect to="/home"/>            
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-          <Footer/>
-      </div>
-    );
+          <p>Loading...</p>
+
+          <Footer />
+        </div>
+      );
+    }else{
+      return (
+        <div>
+          <Header />
+
+            {products.products.map(product =>
+              <div className="products" key={product.name} >
+                <h3>{product.name}</h3>
+                <img className="product" src={product.url_image}></img>
+                <h5>Price: {product.price}$</h5>
+                <h5>Stock: {product.stock} units</h5>
+                <h5>Review: {product.review} 	&#9734;</h5>
+
+              </div>
+            )}
+
+
+          <Footer />
+        </div>
+      );
+    }
+
 }
 }
 
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
+export default withRouter(Main);
